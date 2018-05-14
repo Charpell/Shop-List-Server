ae025730a0ff7f4d88ff41e076390c0359fa2db7
Charpell <charpellumeh@gmail.com>
2018-05-14 07:45:49 +0100

    Create first hello World URL

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from a Severless Database!");
+});
