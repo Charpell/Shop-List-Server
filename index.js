271b3337107e32b76c833fdf04d3b2c769145508
Charpell <charpellumeh@gmail.com>
2018-05-14 07:46:54 +0100

    Create POST addItem URL

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+const cors = require('cors')({ origin: true });
+
+admin.initializeApp();
+
+const database = admin.database().ref('/items');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -8,5 +14,25 @@ const functions = require('firebase-functions');
 // });
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from a Severless Database!");
+  response.send("Hello from a Severless Database!");
 });
+
+
+exports.addItem = functions.https.onRequest((req, res) => {
+  return cors(req, res, () => {
+    if (req.method !== "POST") {
+      return res.status(500).json({
+        message: "Not allowed"
+      });
+    }
+    const items = req.query.items || "Unknown";
+
+    database.push({ items });
+
+    let shopItems = [];
+
+    getItemsFromDatabase(res);
+  });
+});
+
+
