4a49a9e51d2b8f02c54038e711fe524b26093a0d
Charpell <charpellumeh@gmail.com>
2018-05-15 09:41:09 +0100

    Setup Cors and Database

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const functions = require("firebase-functions");
+const cors = require('cors')({ origin: true });
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+const database = admin.database().ref('/items');
 
 
 // // Create and Deploy Your First Cloud Functions
