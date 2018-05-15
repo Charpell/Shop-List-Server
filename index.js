42de3a523d896827469d70c1127038521d428988
Charpell <charpellumeh@gmail.com>
2018-05-15 11:21:05 +0100

    Setup POST /YOUR_URL/addItem function

--- index.js
+++ index.js
@@ -20,3 +20,34 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 
 
 
+exports.addItem = functions.https.onRequest((req, res) => {
+  return cors(req, res, () => {
+    if(req.method !== 'POST') {
+      return res.status(500).json({
+        message: 'Not allowed'
+      })
+    }
+    console.log(req.query)
+  
+    const item = req.query.item
+
+    database.push({ item });
+
+    let items = [];
+
+    return database.on('value', (snapshot) => {
+      snapshot.forEach((item) => {
+        items.push({
+          id: item.key,
+          items: item.val().item
+        });
+      });
+      
+      res.status(200).json(items)
+    }, (error) => {
+      res.status(500).json({
+        message: `Something went wrong. ${error}`
+      })
+    })
+  })
+})
