fb1f8dcf44bdb1baa13b43013df3b7d2edb4f93b
Charpell <charpellumeh@gmail.com>
2018-05-15 08:54:56 +0100

    Create helloWorld function

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const functions = require("firebase-functions");
-const admin = require('firebase-admin');
-const cors = require('cors')({ origin: true });
 
-admin.initializeApp();
-
-const database = admin.database().ref('/items');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -17,22 +12,5 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from a Severless Database!");
 });
 
-
-exports.addItem = functions.https.onRequest((req, res) => {
-  return cors(req, res, () => {
-    if (req.method !== "POST") {
-      return res.status(500).json({
-        message: "Not allowed"
-      });
-    }
-    const items = req.query.items || "Unknown";
-
-    database.push({ items });
-
-    let shopItems = [];
-
-    getItemsFromDatabase(res);
-  });
-});
 
 
